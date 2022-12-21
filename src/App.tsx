52bc8f1635533b31bf17f4c2d36acee1d1f4d950
X-Bytes2048 <kazuharu.nagamura@gmail.com>
2022-12-21 16:26:20 +0900

    setup grid view

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={8}>
+                <p>hi</p>
+              </Grid>
+            </Grid>
+        </Box>
+      </div>
+    );
+  }
 }
 
 export default App;
